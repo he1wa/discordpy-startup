b78f1221d35bbe4d101cc18863fe57c2c3bdac3e
he1wa <84513949+he1wa@users.noreply.github.com>
2021-05-20 23:43:41 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -24,4 +24,4 @@ for bad_word in bad_words:
 loop.start()
 
 
-bot.run(ODQ0OTI1MzQ5MzA4NTMwNjkx.YKZgDQ.Oc0fxZhfbCWMHA3YspGQlGwCZdI)
+bot.run(ODQ0OTI1MzQ5MzA4NTMwNjkx.YKZgDQ.ABMtfswHADkyBpAP-FokXcFeG5Y)
